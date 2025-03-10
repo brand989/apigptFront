d1c6d6acda53069419150d2c786df537472f0f62
brand989 <brand989@gmail.com>
2025-03-10 13:25:17 +0100

    Resolved merge conflict in WebSocketProvider.jsx

--- src/WebSocketProvider.jsx
+++ src/WebSocketProvider.jsx
@@ -2,11 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { checkAuth } from "./api";
 
 const WebSocketContext = createContext(null);
-<<<<<<< HEAD
-const wsUrl = import.meta.env.VITE_WS_URL;
-=======
 const wsUrl = import.meta.env.VITE_WS_URL || "ws://localhost:3000";
->>>>>>> heroku/main
 
 export const WebSocketProvider = ({ children, authenticated  }) => {
   const [socket, setSocket] = useState(null);
