36ddf1546c425607062f920516073d64b6ceca81
brand989 <brand989@gmail.com>
2025-03-11 10:32:50 +0100

    update viteconfig secure true

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
       "/api": {
         target: process.env.VITE_BACKEND_URL || "http://localhost:3000",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
